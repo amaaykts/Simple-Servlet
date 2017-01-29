a1b766360d461a0777be562c6684c0aaf5764a2a
amaaykts <amaaykts@yandex.ru>
2017-01-29 23:19:54 +0300

    Simple example Servlet

---
+++ src/MyServlet.java
@@ -0,0 +1,20 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+
+public class MyServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter printWriter = response.getWriter();
+        printWriter.write("Hello");
+        printWriter.flush();
+        printWriter.close();
+    }
+}
